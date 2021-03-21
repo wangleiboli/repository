accf593b81b5b1692cbea2a2aa02ee93c68e9712
lei.wang <394254381@qq.com>
2021-03-21 21:06:12 +0800

    submit

--- myweb/WebContent/page/js/index.js
+++ myweb/WebContent/page/js/index.js
@@ -14,6 +14,9 @@ $().ready(function() {
 		//	console.log(e.type);
 	});
 
+	/**
+	 * 读取
+	 */
 	$('#read').on('click', function() {
 		var pathname = $('#pathname').val();
 		var data = { pathname: pathname };
@@ -25,12 +28,50 @@ $().ready(function() {
 			data: data,
 			contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
 			success: function(result) {
-				console.log(result);
-				var html_role = $("#role").render(result.configFileList);
-				$("#roles").html(html_role);
+				//console.log(result);
+				var html_role = $('#role').render(result.configFileList);
+				$('#roles').html(html_role);
 
-				var html_item = $("#item").render(result.itemguolvList);
-				$("#items").html(html_item);
+				var html_item = $('#item').render(result.itemguolvList);
+				$('#items').html(html_item);
+			}
+		});
+
+	});
+
+
+	$('#save').on('click', function() {
+
+		var data = new Array();
+
+		$('.item').each(function() {
+
+			var item = {
+				objname: $(this).find('.ObjName').text(),
+				bsell: $(this).find('.bSell input[type="checkbox"]').is(":checked"),
+				bstore: $(this).find('.bStore input[type="checkbox"]').is(":checked"),
+				bdrop: $(this).find('.bDrop input[type="checkbox"]').is(":checked"),
+				iqianghuacishu: $(this).find('.iQiangHuaCishu input[type="text"]').val() * 1,
+				imasu: $(this).find('.iMaSu input[type="text"]').val() * 1,
+				ifangyu: $(this).find('.iFangyu input[type="text"]').val() * 1,
+				igongji: $(this).find('.iGongji input[type="text"]').val() * 1,
+				ishenshangliuliang: $(this).find('.iShenshangliuliang input[type="text"]').val() * 1,
+				iwujiangji: $(this).find('.iWujiangji input[type="text"]').val() * 1,
+				ilev: $(this).find('.iLev input[type="text"]').val() * 1,
+				blevGt: $(this).find('.iLev select').val() == 'true' ? true : false
+			};
+
+			data.push(item);
+		});
+		console.log({ itemguolvList: data });
+		$.ajax({
+			url: 'item/saveConfig.do',
+			type: 'POST',
+			dataType: 'json',
+			data: JSON.stringify(data),
+			contentType: 'application/json;charset=UTF-8',
+			success: function(result) {
+				console.log(result);
 			}
 		});
 
--- myweb/src/main/java/org/boli/myweb/controller/ItemController.java
+++ myweb/src/main/java/org/boli/myweb/controller/ItemController.java
@@ -1,9 +1,13 @@
 package org.boli.myweb.controller;
 
+import java.util.List;
+
+import org.boli.myweb.entity.Itemguolv;
 import org.boli.myweb.model.Config;
 import org.boli.myweb.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,6 +23,14 @@ public class ItemController {
 	public Config readConfig(String pathname) {
 
 		return itemService.readConfig(pathname);
+
+	}
+
+	@RequestMapping("/saveConfig")
+	@ResponseBody
+	public String saveConfig(@RequestBody List<Itemguolv> itemguolvList) {
+
+		return "SUCCESS";
 
 	}
 
