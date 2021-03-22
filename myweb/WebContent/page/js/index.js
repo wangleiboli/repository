4949d5636107fde6bcba77397018eef084173a85
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 16:15:33 +0800

    Update index.js
    s

--- myweb/WebContent/page/js/index.js
+++ myweb/WebContent/page/js/index.js
@@ -8,17 +8,27 @@ $().ready(function() {
 	 * 数字输入框
 	 */
 	$('input.num').on('keyup change', function() {
-		//debugger;
+		// debugger;
 		var value = $(this).val().replace(/[^\d]/g, '');
 		$(this).val(value);
-		//	console.log(e.type);
+		// console.log(e.type);
 	});
+	
+	/**
+	 * 缓存读取配置目录
+	 */
+	var pathname = localStorage.getItem('pathname');
+	if(pathname){
+		$('#pathname').val(pathname);
+	}
 
 	/**
 	 * 读取
 	 */
 	$('#read').on('click', function() {
 		var pathname = $('#pathname').val();
+		// 配置目录写缓存
+		localStorage.setItem('pathname', pathname);
 		var data = { pathname: pathname };
 
 		$.ajax({
@@ -29,49 +39,54 @@ $().ready(function() {
 			contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
 			success: function(result) {
 				//console.log(result);
-				var html_role = $('#role').render(result.configFileList);
+				var html_role = $('#role').render(result.data.configFileList);
 				$('#roles').html(html_role);
 
-				var html_item = $('#item').render(result.itemguolvList);
+				var html_item = $('#item').render(result.data.itemguolvList);
 				$('#items').html(html_item);
 			}
 		});
 
 	});
 
-
+	/**
+	 * 保存
+	 */
 	$('#save').on('click', function() {
 
-		var data = new Array();
+		var data = {};
+		
+		$('.role').each(function(index) {
+			
+			data['configFileList['+index+'].filePath'] = $(this).find('.filePath').text();
 
-		$('.item').each(function() {
-
-			var item = {
-				objname: $(this).find('.ObjName').text(),
-				bsell: $(this).find('.bSell input[type="checkbox"]').is(":checked"),
-				bstore: $(this).find('.bStore input[type="checkbox"]').is(":checked"),
-				bdrop: $(this).find('.bDrop input[type="checkbox"]').is(":checked"),
-				iqianghuacishu: $(this).find('.iQiangHuaCishu input[type="text"]').val() * 1,
-				imasu: $(this).find('.iMaSu input[type="text"]').val() * 1,
-				ifangyu: $(this).find('.iFangyu input[type="text"]').val() * 1,
-				igongji: $(this).find('.iGongji input[type="text"]').val() * 1,
-				ishenshangliuliang: $(this).find('.iShenshangliuliang input[type="text"]').val() * 1,
-				iwujiangji: $(this).find('.iWujiangji input[type="text"]').val() * 1,
-				ilev: $(this).find('.iLev input[type="text"]').val() * 1,
-				blevGt: $(this).find('.iLev select').val() == 'true' ? true : false
-			};
-
-			data.push(item);
 		});
-		console.log({ itemguolvList: data });
+
+		$('.item').each(function(index) {
+			
+			data['itemguolvList['+index+'].objname'] = $(this).find('.ObjName').text();
+			data['itemguolvList['+index+'].bsell'] = $(this).find('.bSell input[type="checkbox"]').is(':checked');
+			data['itemguolvList['+index+'].bstore'] = $(this).find('.bStore input[type="checkbox"]').is(':checked');
+			data['itemguolvList['+index+'].bdrop'] = $(this).find('.bDrop input[type="checkbox"]').is(':checked');
+			data['itemguolvList['+index+'].iqianghuacishu'] = $(this).find('.iQiangHuaCishu input[type="text"]').val();
+			data['itemguolvList['+index+'].imasu'] = $(this).find('.iMaSu input[type="text"]').val();
+			data['itemguolvList['+index+'].ifangyu'] = $(this).find('.iFangyu input[type="text"]').val();
+			data['itemguolvList['+index+'].igongji'] = $(this).find('.iGongji input[type="text"]').val();
+			data['itemguolvList['+index+'].ishenshangliuliang'] = $(this).find('.iShenshangliuliang input[type="text"]').val();
+			data['itemguolvList['+index+'].iwujiangji'] = $(this).find('.iWujiangji input[type="text"]').val();
+			data['itemguolvList['+index+'].ilev'] = $(this).find('.iLev input[type="text"]').val();
+			data['itemguolvList['+index+'].blevGt'] = $(this).find('.iLev select').val();
+
+		});
+		
 		$.ajax({
 			url: 'item/saveConfig.do',
 			type: 'POST',
 			dataType: 'json',
-			data: JSON.stringify(data),
-			contentType: 'application/json;charset=UTF-8',
+			data: data,
+			contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
 			success: function(result) {
-				console.log(result);
+				alert(result.status);
 			}
 		});
 
