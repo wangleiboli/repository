39f7335bfcc1a92d835572e2ac8387adbeb9fca3
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 10:57:13 +0800

    Create Config.java
    s

---
+++ myweb/src/main/java/org/boli/myweb/pojo/Config.java
@@ -0,0 +1,15 @@
+package org.boli.myweb.pojo;
+
+public class Config {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
