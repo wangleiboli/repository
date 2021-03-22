a7bab5f2c81086489bcdfd75b5e07726da212c0f
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 12:17:35 +0800

    Update User.java
    s

--- myweb/src/main/java/org/boli/myweb/entity/User.java
+++ myweb/src/main/java/org/boli/myweb/entity/User.java
@@ -1,33 +1,33 @@
 package org.boli.myweb.entity;
 
 public class User {
-    private Integer id;
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private String address;
+	private String address;
 
-    public Integer getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
+	public void setName(String name) {
+		this.name = name == null ? null : name.trim();
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
+	public void setAddress(String address) {
+		this.address = address == null ? null : address.trim();
+	}
 }
