75b20a9a55d00ae90a59e2e65266d547c6481d44
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 16:08:00 +0800

    Update Config.java
    s

--- myweb/src/main/java/org/boli/myweb/model/Config.java
+++ myweb/src/main/java/org/boli/myweb/model/Config.java
@@ -1,10 +1,7 @@
 package org.boli.myweb.model;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.boli.myweb.entity.Itemguolv;
 
@@ -12,9 +9,7 @@ public class Config {
 
 	List<ConfigFile> configFileList = new ArrayList<ConfigFile>();
 
-	Map<String, Itemguolv> itemguolvMap = new HashMap<String, Itemguolv>();
-
-	Collection<Itemguolv> itemguolvList = new ArrayList<Itemguolv>();
+	List<Itemguolv> itemguolvList = new ArrayList<Itemguolv>();
 
 	public List<ConfigFile> getConfigFileList() {
 		return configFileList;
@@ -24,19 +19,11 @@ public class Config {
 		this.configFileList = configFileList;
 	}
 
-	public Map<String, Itemguolv> getItemguolvMap() {
-		return itemguolvMap;
+	public List<Itemguolv> getItemguolvList() {
+		return itemguolvList;
 	}
 
-	public void setItemguolvMap(Map<String, Itemguolv> itemguolvMap) {
-		this.itemguolvMap = itemguolvMap;
-	}
-
-	public Collection<Itemguolv> getItemguolvList() {
-		return itemguolvMap.values();
-	}
-
-	public void setItemguolvList(Collection<Itemguolv> itemguolvList) {
+	public void setItemguolvList(List<Itemguolv> itemguolvList) {
 		this.itemguolvList = itemguolvList;
 	}
 
