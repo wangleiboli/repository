04b38d0eb826a3e3fb58ab9a4ac3199397aa5a43
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 16:14:36 +0800

    Update ItemService.java
    s

--- myweb/src/main/java/org/boli/myweb/service/ItemService.java
+++ myweb/src/main/java/org/boli/myweb/service/ItemService.java
@@ -4,6 +4,19 @@ import org.boli.myweb.model.Config;
 
 public interface ItemService {
 
+	/**
+	 * 读取物品过滤信息
+	 * 
+	 * @param pathname
+	 * @return
+	 */
 	Config readConfig(String pathname);
+
+	/**
+	 * 保存物品过滤信息
+	 * 
+	 * @param config
+	 */
+	void saveConfig(Config config);
 
 }
