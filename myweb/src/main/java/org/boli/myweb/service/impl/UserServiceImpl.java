01aec1c66075490015e21c77dbe775569c0d4e48
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-22 11:50:00 +0800

    Update UserServiceImpl.java
    s

--- myweb/src/main/java/org/boli/myweb/service/impl/UserServiceImpl.java
+++ myweb/src/main/java/org/boli/myweb/service/impl/UserServiceImpl.java
@@ -8,28 +8,47 @@ import org.boli.myweb.entity.UserExample.Criteria;
 import org.boli.myweb.mapper.UserMapper;
 import org.boli.myweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+/**
+ * {@link Component @Component}注解的类，相当于xml中配置的bean<br>
+ * Spring提供了{@link Component @Component}的三个衍生注解<br>
+ * {@link Controller @Controller}通常用于Controller类，也就是控制层<br>
+ * {@link Service @Service}通常用于注解Service类，也就是服务层<br>
+ * {@link Repository @Repository}通常用于注解DAO类，也就是我们常说的持久层<br>
+ * 
+ * <p>
+ * 自动装配出现多个bean候选者时，被注解为{@link Primary @Primary}的bean将作为首选者，否则将抛出异常。
+ */
 @Service
+@Primary
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserMapper userMapper;
 
 	@Override
-	public User selectById(int id) {
-		return userMapper.selectByPrimaryKey(id);
+	public PageInfo<User> queryAll() {
+
+		PageHelper.startPage(1, 10);
+		List<User> usersList = userMapper.selectByExample(null);
+		PageInfo<User> pageInfo = new PageInfo<User>(usersList);
+
+		return pageInfo;
 	}
 
 	@Override
-	public List<User> select(User user) {
+	public List<User> query(User user) {
 
 		UserExample example = new UserExample();
 		Criteria criteria = example.createCriteria();
-		if (!user.getName().isEmpty()) {
+		criteria.andIdEqualTo(user.getId());
+		if (user.getName() != null) {
 			criteria.andNameLike("%" + user.getName() + "%");
 		}
 
@@ -37,13 +56,8 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public PageInfo<User> selectAll() {
-
-		PageHelper.startPage(1, 10);
-
-		List<User> userList = userMapper.selectByExample(null);
-
-		return new PageInfo<User>(userList);
+	public User get(long id) {
+		return userMapper.selectByPrimaryKey(id);
 	}
 
 }
