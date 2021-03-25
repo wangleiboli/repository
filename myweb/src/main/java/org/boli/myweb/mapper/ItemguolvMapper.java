b884d6b94b12f2632e4c1b761ef3acdef117b841
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-25 16:44:08 +0800

    Update ItemguolvMapper.java
    s

--- myweb/src/main/java/org/boli/myweb/mapper/ItemguolvMapper.java
+++ myweb/src/main/java/org/boli/myweb/mapper/ItemguolvMapper.java
@@ -6,17 +6,17 @@ import org.boli.myweb.entity.Itemguolv;
 import org.boli.myweb.entity.ItemguolvExample;
 
 public interface ItemguolvMapper {
-    long countByExample(ItemguolvExample example);
+	long countByExample(ItemguolvExample example);
 
-    int deleteByExample(ItemguolvExample example);
+	int deleteByExample(ItemguolvExample example);
 
-    int insert(Itemguolv record);
+	int insert(Itemguolv record);
 
-    int insertSelective(Itemguolv record);
+	int insertSelective(Itemguolv record);
 
-    List<Itemguolv> selectByExample(ItemguolvExample example);
+	List<Itemguolv> selectByExample(ItemguolvExample example);
 
-    int updateByExampleSelective(@Param("record") Itemguolv record, @Param("example") ItemguolvExample example);
+	int updateByExampleSelective(@Param("record") Itemguolv record, @Param("example") ItemguolvExample example);
 
-    int updateByExample(@Param("record") Itemguolv record, @Param("example") ItemguolvExample example);
+	int updateByExample(@Param("record") Itemguolv record, @Param("example") ItemguolvExample example);
 }
