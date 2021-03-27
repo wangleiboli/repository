7d74bdd21e6454612e667d2f0809fd1b94d9cb5e
wangleiboli <80578957+wangleiboli@users.noreply.github.com>
2021-03-27 15:57:49 +0800

    Update ItemService.java
    s

--- myweb/src/main/java/org/boli/myweb/service/ItemService.java
+++ myweb/src/main/java/org/boli/myweb/service/ItemService.java
@@ -1,6 +1,7 @@
 package org.boli.myweb.service;
 
 import org.boli.myweb.model.Config;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ItemService {
 
@@ -8,9 +9,10 @@ public interface ItemService {
 	 * 读取物品过滤信息
 	 * 
 	 * @param pathname
+	 * @param files
 	 * @return
 	 */
-	Config readConfig(String pathname);
+	Config readConfig(String pathname, MultipartFile[] files);
 
 	/**
 	 * 保存物品过滤信息
